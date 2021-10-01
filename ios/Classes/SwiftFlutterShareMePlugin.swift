e94343ba13481740d9afbbc098c27cc575091d17
sivsushruth <sivsushruth@gmail.com>
2021-10-01 17:21:59 +0530

    remove fb from ios

--- ios/Classes/SwiftFlutterShareMePlugin.swift
+++ ios/Classes/SwiftFlutterShareMePlugin.swift
@@ -1,8 +1,7 @@
 import Flutter
 import UIKit
-import FBSDKShareKit
 import PhotosUI
-public class SwiftFlutterShareMePlugin: NSObject, FlutterPlugin, SharingDelegate {
+public class SwiftFlutterShareMePlugin: NSObject, FlutterPlugin {
     
     
     let _methodWhatsApp = "whatsapp_share";
@@ -37,42 +36,6 @@ public class SwiftFlutterShareMePlugin: NSObject, FlutterPlugin, SharingDelegate
                 shareWhatsApp(message: args!["msg"] as! String,imageUrl: args!["url"] as! String,type: args!["fileType"] as! String,result: result)
             }
             
-        }
-        else if(call.method.elementsEqual(_methodWhatsAppBusiness)){
-            
-            // There is no way to open WB in IOS.
-            result(FlutterMethodNotImplemented)
-            
-            //            let args = call.arguments as? Dictionary<String,Any>
-            //
-            //            if args!["url"]as! String == "" {
-            //                // if don't pass url then pass blank so if can strat normal whatsapp
-            //                shareWhatsApp4Biz(message: args!["msg"] as! String, result: result)
-            //            }else{
-            //                // if user pass url then use that
-            //                // wil open share sheet and user can select open for there.
-            //                //                shareWhatsApp(message: args!["msg"] as! String,imageUrl: args!["url"] as! String,result: result)
-            //            }
-        }
-        else if(call.method.elementsEqual(_methodWhatsAppPersonal)){
-            let args = call.arguments as? Dictionary<String,Any>
-            shareWhatsAppPersonal(message: args!["msg"]as! String, phoneNumber: args!["phoneNumber"]as! String, result: result)
-        }
-        else if(call.method.elementsEqual(_methodFaceBook)){
-            let args = call.arguments as? Dictionary<String,Any>
-            sharefacebook(message: args!, result: result)
-            
-        }else if(call.method.elementsEqual(_methodTwitter)){
-            let args = call.arguments as? Dictionary<String,Any>
-            shareTwitter(message: args!["msg"] as! String, url: args!["url"] as! String, result: result)
-        }
-        else if(call.method.elementsEqual(_methodInstagram)){
-            let args = call.arguments as? Dictionary<String,Any>
-            shareInstagram(args: args!)
-        }
-        else if(call.method.elementsEqual(_methodTelegramShare)){
-            let args = call.arguments as? Dictionary<String,Any>
-            shareToTelegram(message: args!["msg"] as! String, result: result )
         }
         else{
             let args = call.arguments as? Dictionary<String,Any>
@@ -145,104 +108,6 @@ public class SwiftFlutterShareMePlugin: NSObject, FlutterPlugin, SharingDelegate
         }
     }
     
-    
-    // Send whatsapp personal message
-    // @ message
-    // @ phone with contry code.
-    func shareWhatsAppPersonal(message:String, phoneNumber:String,result: @escaping FlutterResult)  {
-        
-        let whatsURL = "whatsapp://send?phone=\(phoneNumber)&text=\(message)"
-        var characterSet = CharacterSet.urlQueryAllowed
-        characterSet.insert(charactersIn: "?&")
-        let whatsAppURL  = NSURL(string: whatsURL.addingPercentEncoding(withAllowedCharacters: characterSet)!)
-        if UIApplication.shared.canOpenURL(whatsAppURL! as URL)
-        {
-            UIApplication.shared.openURL(whatsAppURL! as URL)
-            result("Sucess");
-        }else{
-            result(FlutterError(code: "Not found", message: "WhatsApp is not found", details: "WhatsApp not intalled or Check url scheme."));
-        }
-    }
-    
-    func shareWhatsApp4Biz(message:String, result: @escaping FlutterResult)  {
-        let whatsApp = "https://wa.me/?text=\(message)"
-        var characterSet = CharacterSet.urlQueryAllowed
-        characterSet.insert(charactersIn: "?&")
-        
-        let whatsAppURL  = NSURL(string: whatsApp.addingPercentEncoding(withAllowedCharacters: characterSet)!)
-        if UIApplication.shared.canOpenURL(whatsAppURL! as URL)
-        {
-            result("Sucess");
-            UIApplication.shared.openURL(whatsAppURL! as URL)
-        }
-        else
-        {
-            result(FlutterError(code: "Not found", message: "WhatsAppBusiness is not found", details: "WhatsAppBusiness not intalled or Check url scheme."));
-        }
-    }
-    // share twitter
-    // params
-    // @ map conting meesage and url
-    
-    func sharefacebook(message:Dictionary<String,Any>, result: @escaping FlutterResult)  {
-        let viewController = UIApplication.shared.delegate?.window??.rootViewController
-        let shareDialog=ShareDialog()
-        let shareContent = ShareLinkContent()
-        shareContent.contentURL = URL.init(string: message["url"] as! String)!
-        shareContent.quote = message["msg"] as? String
-        shareDialog.mode = .automatic
-        ShareDialog(fromViewController: viewController, content: shareContent, delegate: self).show()
-        result("Sucess")
-        
-    }
-    
-    // share twitter params
-    // @ message
-    // @ url
-    func shareTwitter(message:String,url:String, result: @escaping FlutterResult)  {
-        let urlstring = url
-        let twitterUrl =  "twitter://post?message=\(message)"
-        
-        let urlTextEscaped = urlstring.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)
-        let url = URL(string: urlTextEscaped ?? "")
-        
-        let urlWithLink = twitterUrl + url!.absoluteString
-        
-        let escapedShareString = urlWithLink.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
-        // cast to an url
-        let urlschme = URL(string: escapedShareString)
-        // open in safari
-        do {
-            if UIApplication.shared.canOpenURL(urlschme! as URL){
-                UIApplication.shared.openURL(urlschme!)
-                result("Sucess")
-            }else{
-                result(FlutterError(code: "Not found", message: "Twitter is not found", details: "Twitter not intalled or Check url scheme."));
-                
-            }
-        }
-        
-    }
-    //share via telegram
-    //@ text that you want to share.
-    func shareToTelegram(message: String,result: @escaping FlutterResult )
-    {
-        let telegram = "tg://msg?text=\(message)"
-        var characterSet = CharacterSet.urlQueryAllowed
-        characterSet.insert(charactersIn: "?&")
-        let telegramURL  = NSURL(string: telegram.addingPercentEncoding(withAllowedCharacters: characterSet)!)
-        if UIApplication.shared.canOpenURL(telegramURL! as URL)
-        {
-            result("Sucess");
-            UIApplication.shared.openURL(telegramURL! as URL)
-        }
-        else
-        {
-            result(FlutterError(code: "Not found", message: "telegram is not found", details: "telegram not intalled or Check url scheme."));
-        }
-    
-    }
-
     //share via system native dialog
     //@ text that you want to share.
     func systemShare(message:String,result: @escaping FlutterResult)  {
@@ -267,65 +132,5 @@ public class SwiftFlutterShareMePlugin: NSObject, FlutterPlugin, SharingDelegate
         
     }
     
-    // share image via instagram stories.
-    // @ args image url
-    func shareInstagram(args:Dictionary<String,Any>)  {
-        let imageUrl=args["url"] as! String
-    
-        let image = UIImage(named: imageUrl)
-        if(image==nil){
-            self.result!("File format not supported Please check the file.")
-            return;
-        }
-        guard let instagramURL = NSURL(string: "instagram://app") else {
-            if let result = result {
-                self.result?("Instagram app is not installed on your device")
-                result(false)
-            }
-            return
-        }
-        
-        do{
-            try PHPhotoLibrary.shared().performChangesAndWait {
-                let request = PHAssetChangeRequest.creationRequestForAsset(from: image!)
-                let assetId = request.placeholderForCreatedAsset?.localIdentifier
-                let instShareUrl:String? = "instagram://library?LocalIdentifier=" + assetId!
-                
-                //Share image
-                if UIApplication.shared.canOpenURL(instagramURL as URL) {
-                    if let sharingUrl = instShareUrl {
-                        if let urlForRedirect = NSURL(string: sharingUrl) {
-                            if #available(iOS 10.0, *) {
-                                UIApplication.shared.open(urlForRedirect as URL, options: [:], completionHandler: nil)
-                            }
-                            else{
-                                UIApplication.shared.openURL(urlForRedirect as URL)
-                            }
-                        }
-                        self.result?("Success")
-                    }
-                } else{
-                    self.result?("Instagram app is not installed on your device")
-                }
-            }
-        
-        } catch {
-            print("Fail")
-        }
-    }
-    
-    //Facebook delegate methods
-    public func sharer(_ sharer: Sharing, didCompleteWithResults results: [String : Any]) {
-        print("Share: Success")
-        
-    }
-    
-    public func sharer(_ sharer: Sharing, didFailWithError error: Error) {
-        print("Share: Fail")
-        
-    }
-    
-    public func sharerDidCancel(_ sharer: Sharing) {
-        print("Share: Cancel")
-    }
+
 }
